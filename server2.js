100ee59b904f077ec7dd3303b461ae0da6c431c5
Daniel <dspinoz@gmail.com>
2015-04-02 09:19:05 -0400

    browser example

---
+++ server2.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+
+app.configure(function() {
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname));
+});
+
+app.get('/', function(req,res) {
+  console.log('GET');
+  res.sendfile('index.html');
+});
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+ 
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production 
+    // applications, as it defeats all standard cross-origin protection 
+    // facilities built into the protocol and the browser.  You should 
+    // *always* verify the connection's origin and decide whether or not 
+    // to accept it. 
+    autoAcceptConnections: false
+});
+ 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed. 
+  return true;
+}
+
+var clients = [];
+ 
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin 
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection from ' +request.origin+ ' accepted.');
+
+    //my client index
+    var index = clients.push(connection) - 1;
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            //the client
+            connection.sendUTF(message.utf8Data);
+
+	    //broadcast to all clients
+	    for(var i=0; i < clients.length; i++) {
+              clients[i].sendUTF(message.utf8Data);
+            }
+
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+
+
